85a32a09568adbd1657103172cad94e462bb09d1
Mahaveer prasad panigrahi <psatyam86619@gmail.com>
2023-05-28 10:22:35 +0530

    Addded Splash screen and getting started page

---
+++ app/src/main/java/com/example/codigoplataforma/Startup/Getting_started_or_login.java
@@ -0,0 +1,50 @@
+package com.example.codigoplataforma.Startup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.codigoplataforma.R;
+
+public class Getting_started_or_login extends AppCompatActivity {
+
+    Button login;
+    Button getStarted;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_getting_started_or_login);
+
+        getWindow().setNavigationBarColor(getResources().getColor(R.color.orange));
+        getWindow().setStatusBarColor(getResources().getColor(R.color.black));
+
+        setContentView(R.layout.activity_getting_started_or_login);
+
+        login = findViewById(R.id.btn_login);
+        getStarted = findViewById(R.id.btn_getstarted);
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Getting_started_or_login.this,Splash_Screen.class);
+                startActivity(i);
+
+            }
+        });
+
+        getStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Getting_started_or_login.this,Splash_Screen.class);
+                startActivity(i);
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/example/codigoplataforma/Startup/Splash_Screen.java
+++ app/src/main/java/com/example/codigoplataforma/Startup/Splash_Screen.java
@@ -2,15 +2,51 @@ package com.example.codigoplataforma.Startup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import com.example.codigoplataforma.R;
 
 public class Splash_Screen extends AppCompatActivity {
 
+    Animation ellipse1,ellipse2,logo;
+    ImageView Iellipse1,Iellipse2,Ilogo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        ellipse1 = AnimationUtils.loadAnimation(this,R.anim.elpise1);
+        ellipse2 = AnimationUtils.loadAnimation(this,R.anim.elipse2);
+        logo = AnimationUtils.loadAnimation(this,R.anim.logo);
+
+
+        Iellipse1 = findViewById(R.id.ellipse_1);
+        Iellipse2 = findViewById(R.id.ellipse_2);
+        Ilogo = findViewById(R.id.logo);
+
+        Iellipse1.setAnimation(ellipse1);
+        Iellipse2.setAnimation(ellipse2);
+        Ilogo.setAnimation(logo);
+
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent i = new Intent(Splash_Screen.this, Getting_started_or_login.class);
+                startActivity(i);
+                finish();
+            }
+        },1700);
+
     }
 }
